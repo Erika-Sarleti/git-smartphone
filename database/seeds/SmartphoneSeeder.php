893da50d1997c5632da10ae0e09fb264e50f8b0c
LLPROG <lucalobianco@hotmail.it>
2022-05-10 17:20:14 +0200

    make seed

--- database/seeds/SmartphoneSeeder.php
+++ database/seeds/SmartphoneSeeder.php
@@ -11,6 +11,37 @@ class SmartphoneSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $smartphones = [
+            [
+                'brand' => 'iphone',
+                'model' => 'x10',
+                'year' => '2020'
+            ],
+            [
+                'brand' => 'nokia',
+                'model' => '3330',
+                'year' => '1995'
+            ],
+            [
+                'brand' => 'huawei',
+                'model' => 'quellobello',
+                'year' => '1565'
+            ],
+            [
+                'brand' => 'samsung',
+                'model' => 'galaxy',
+                'year' => '2000'
+            ],
+            [
+                'brand' => 'motorola',
+                'model' => '200',
+                'year' => '1998'
+            ],
+            [
+                'brand' => 'unaltro',
+                'model' => 'nono',
+                'year' => '1250'
+            ],
+        ];
     }
 }
